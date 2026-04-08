577a02c866cba67208e29308be64bc13d98b4639
Saul Enriquez <saul.enriquez2005@gmail.com>
2026-04-08 10:38:20 -0500

    refactor(validation): remove common password check
    - The hardcoded blocklist was too small to provide actual security

--- src/vault/utils/password_validator.py
+++ src/vault/utils/password_validator.py
@@ -19,10 +19,6 @@ class PasswordStrength(IPasswordValidator):
     def _validate_has_special_char(self, password: str) -> tuple[bool, str]:
         return (bool(re.search(r"[ !#$%&'()*+,-./:;<=>?@[\]^_`{|}~]", password)), "Add special chars (@, #, $, etc.)")
 
-    def _validate_not_common(self, password: str) -> tuple[bool, str]:
-        common = {"password", "admin"}
-        return (password.lower() not in common, "Avoid common passwords")
-
 
     def validate_password(self, password: str) -> tuple[int, list[str]]:
         score = 0
@@ -32,8 +28,7 @@ class PasswordStrength(IPasswordValidator):
             self._validate_min_length,
             self._validate_has_number,
             self._validate_mixed_case,
-            self._validate_has_special_char,
-            self._validate_not_common
+            self._validate_has_special_char
         ]:
 
             passed, msg = rule(password)
